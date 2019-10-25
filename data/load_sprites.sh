3206969698d2153e07c80940004b01c5886d372d
Dmitry Baranchuk <dmitrybaranchuk@gmail.com>
2019-10-25 16:29:28 +0300

    Update load_sprites.sh

--- data/load_sprites.sh
+++ data/load_sprites.sh
@@ -1,4 +1,4 @@
 DATA_DIR="data/sprites"
 
 mkdir -p ${DATA_DIR}
-wget https://www.dropbox.com/s/cjuzj71v5sgwcge/sprites.npz?dl=1 -O ${DATA_DIR}/sprites.zip
+wget https://www.dropbox.com/s/cjuzj71v5sgwcge/sprites.npz?dl=1 -O ${DATA_DIR}/sprites.npz
